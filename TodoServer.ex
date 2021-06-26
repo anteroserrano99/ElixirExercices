c31e08bfcd18f09e1daaf350edf6cc0052c7cf76
anteroserrano99 <serranoantero13@gmail.com>
2021-06-26 18:40:40 +0200

    Concurrent todoList using a TodoServer to proces the request

--- TodoServer.ex
+++ TodoServer.ex
@@ -1,3 +1,38 @@
+defmodule TodoServer do
+
+  def start do
+    spawn(fn -> loop(TodoList.new())end)
+  end
+
+  defp loop(todo_list) do
+    new_todo_list = receive do
+      message -> process_message(todo_list, message)
+    end
+
+    loop(new_todo_list)
+  end
+
+  def process_message(todo_list, {:retrieve, caller}) do
+    send(caller, todo_list)
+  end
+
+  def process_message(todo_list, {:add_entry, new_entry}) do
+    TodoList.addEntry(todo_list, new_entry)
+  end
+
+  def process_message(todo_list, {:update_entry, id, update_funciton}) do
+    TodoList.updateEntry(todo_list, id, update_funciton)
+  end
+
+  def process_message(todo_list, {:delete_entry, id}) do
+    TodoList.deleteEntry(todo_list, id)
+  end
+
+
+
+end
+
+
 
 defmodule TodoList do
 
@@ -54,31 +89,33 @@ defmodule TodoList do
 
   end
 
-  defmodule CsvImporter do
-    def openCsv(path) do
-      File.stream!(path)
-      |> Stream.map(&String.replace(&1, "\n", ""))
-      |> Stream.map(&String.split(&1,","))
-      |> Stream.map(&parseEntry(&1))
-      |> Enum.to_list()
-      |> TodoList.addNewEntries()
 
+end
+
+
+defmodule TestingModule do
+
+  def test_server_add() do
+    pid = TodoServer.start()
+    todo_list = generate_test_data()
+    caller = self()
+
+    send(pid, {:add_entry, todo_list.entries[1]})
+    send(pid, {:add_entry, todo_list.entries[1]})
+    send(pid, {:add_entry, todo_list.entries[1]})
+    send(pid, {:update_entry, 1, &Map.put(&1, :title, "you are free today")})
+    send(pid, {:delete_entry, 3})
+    send(pid, {:retrieve, caller})
+
+    ## wait for the reception of the retrieve proccess message
+    receive do
+      list -> list
     end
 
-    defp parseEntry([date, title]) do
-      {year, month, day} = String.split(date, "/")
-      |> List.to_tuple()
-
-      parsedDate = year <> "-" <> month <> "-" <> day
-      {:ok, parsedDate} = Date.from_iso8601(parsedDate)
-
-      %{date: parsedDate, title: title}
-    end
 
   end
 
 
-  defmodule TestingModule do
 
   def generate_test_data() do
     entry1 = %{date: ~D[2021-01-01], title: "theater"}
@@ -90,19 +127,4 @@ defmodule TodoList do
     |> TodoList.addEntry(entry3)
 
   end
-
-  def update_element_test() do
-
-    todo_list = generate_test_data()
-    |> TodoList.updateEntry(1, &Map.put(&1, :date, ~D[2111-01-01]) )
-    |> TodoList.updateEntry(2, &Map.put(&1, :title, "you are free today"))
-  end
-
-  def delete_test() do
-    generate_test_data()
-    |> TodoList.deleteEntry(1)
-  end
-
-end
-
 end
