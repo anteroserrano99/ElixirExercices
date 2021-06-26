71af135c2c3a98ff3f0d3b311d7dc4dd2deda40b
anteroserrano99 <serranoantero13@gmail.com>
2021-06-26 12:11:11 +0200

    TodoList added

---
+++ TodoList.ex
@@ -0,0 +1,70 @@
+
+defmodule TodoList do
+
+  defstruct  autoId: 1, entries: %{}
+
+
+  def new, do: %TodoList{}
+
+  def addEntry(todo_list, entry) do
+    entry = Map.put(entry, :id, todo_list.autoId)
+    new_entries = Map.put(todo_list.entries, todo_list.autoId, entry)
+    %TodoList{ todo_list |
+      entries: new_entries,
+      autoId: todo_list.autoId + 1
+  }
+
+  end
+
+  def updateEntry(todo_list, id, updateFunction) do
+    case Map.fetch(todo_list.entries, id) do
+
+      :error -> todo_list
+
+      {:ok, old_entry} ->
+        new_entry = updateFunction.(old_entry)
+        new_entries = Map.put(todo_list.entries, id, new_entry)
+        %TodoList{todo_list |
+          entries: new_entries
+        }
+    end
+
+  end
+
+  def deleteEntry(todo_list, id) do
+    case Map.fetch(todo_list.entries, id) do
+      :error -> todo_list
+      {:ok, entry_to_delete} ->
+      new_entries = Map.delete(todo_list.entries, id)
+      %TodoList{todo_list |
+        entries: new_entries
+    }
+    end
+
+  end
+
+
+  def generate_test_data() do
+    entry1 = %{date: ~D[2021-01-01], title: "theater"}
+    entry2 = %{date: ~D[2021-01-02], title: "park"}
+    entry3 = %{date: ~D[2021-01-03], title: "cinema"}
+    TodoList.new()
+    |> TodoList.addEntry(entry1)
+    |> TodoList.addEntry(entry2)
+    |> TodoList.addEntry(entry3)
+
+  end
+
+  def update_element_test() do
+
+    todo_list = generate_test_data()
+    |> TodoList.updateEntry(1, &Map.put(&1, :date, ~D[2111-01-01]) )
+    |> TodoList.updateEntry(2, &Map.put(&1, :title, "you are free today"))
+  end
+
+  def delete_test() do
+    generate_test_data()
+    |> deleteEntry(1)
+  end
+
+end
