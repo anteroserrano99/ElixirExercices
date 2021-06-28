2f1241eb99f8566d4b47ab94512a343ac7d56b7c
anteroserrano99 <serranoantero13@gmail.com>
2021-06-28 22:59:23 +0200

    Added KeyValue generic server with OTP implementation

---
+++ KeyValueGenServer.ex
@@ -0,0 +1,56 @@
+
+
+
+
+defmodule KeyValueStore do
+  use GenServer
+
+  def init(_) do
+    :timer.send_interval(5000, :cleanup)
+    {:ok, %{}}
+  end
+
+
+  def start() do
+    GenServer.start(KeyValueStore, nil)
+  end
+
+  def put(pid, key, value) do
+    GenServer.cast(pid, {:put, key, value})
+  end
+
+  def get(pid, key) do
+    GenServer.call(pid, {:get, key})
+  end
+
+
+
+  def handle_cast({:put, key, value}, state) do
+    {:noreply, Map.put(state, key, value)}
+  end
+
+  def handle_call({:get, key}, _, state) do
+    {:reply, Map.get(state, key), state}
+  end
+
+
+  def handle_info(:cleanup, state) do
+    IO.puts( "Performing cleanup")
+    {:noreply, state}
+  end
+
+
+
+
+end
+
+defmodule TestModule do
+
+  def test_key_value() do
+    {:ok, pid} = KeyValueStore.start()
+    KeyValueStore.put(pid, :key, :value)
+    KeyValueStore.get(pid, :key)
+
+  end
+
+end
