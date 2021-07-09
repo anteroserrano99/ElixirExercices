6baf0dba903627c451fd8f80ec99000cbbc4ea6b
anteroserrano99 <serranoantero13@gmail.com>
2021-07-09 17:46:54 +0200

    idle timeout added in order to delete Todo.Server procces if no action is performed

--- supervised_todo_cache/lib/todo/server.ex
+++ supervised_todo_cache/lib/todo/server.ex
@@ -1,9 +1,11 @@
 defmodule Todo.Server do
   use GenServer, restart: :temporary
 
+  @expiry_idle_timeout :timer.seconds(10)
+
   @impl GenServer
   def init(name) do
-    {:ok, {name, Todo.List.new() || Todo.List.new()}}
+    {:ok, {name, Todo.List.new() || Todo.List.new()}, @expiry_idle_timeout}
   end
 
   def start_link(name) do
@@ -34,30 +36,36 @@ defmodule Todo.Server do
     Todo.ProcessRegistry.via_tuple({__MODULE__, name})
   end
 
+  def handle_info(:timeout, {name, todo_list}) do
+    IO.puts("Stopping Server #{name}")
+    {:stop, :normal, {name, todo_list}}
+
+  end
+
   @impl GenServer
   def handle_call({:retrieve}, _, {name, todo_list}) do
-    {:reply, Todo.Database.get(name), todo_list}
+    {:reply, Todo.Database.get(name), todo_list, @expiry_idle_timeout}
   end
 
   @impl GenServer
   def handle_cast({:add_entry, new_entry}, {name, todo_list}) do
     new_list = Todo.List.addEntry(todo_list, new_entry)
     Todo.Database.store(name, new_list)
-    {:noreply, {name, new_list}}
+    {:noreply, {name, new_list}, @expiry_idle_timeout}
   end
 
   @impl GenServer
   def handle_cast({:update_entry, id, update_funciton}, {name, todo_list}) do
     new_list = Todo.List.updateEntry(todo_list, id, update_funciton)
     Todo.Database.store(name, new_list)
-    {:noreply, {name, new_list}}
+    {:noreply, {name, new_list}, @expiry_idle_timeout}
   end
 
   @impl GenServer
   def handle_cast({:delete_entry, id}, {name, todo_list}) do
     new_list = Todo.List.deleteEntry(todo_list, id)
     Todo.Database.store(name, new_list)
-    {:noreply, {name, new_list}}
+    {:noreply, {name, new_list}, @expiry_idle_timeout}
   end
 
 end
