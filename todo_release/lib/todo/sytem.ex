0971d301cd827beb7bc308922c795f2db57d49f6
anteroserrano99 <serranoantero13@gmail.com>
2021-07-11 13:19:23 +0200

    added project for OTP release

---
+++ todo_release/lib/todo/sytem.ex
@@ -0,0 +1,13 @@
+defmodule Todo.System do
+
+  def start_link do
+    Supervisor.start_link([
+      Todo.Database,
+#      Todo.Metrics,
+      Todo.Cache,
+      Todo.Web
+      ],
+        strategy: :one_for_one)
+  end
+
+end
